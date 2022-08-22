03844005297ba5909bb5fae8b7aaf1f0a84b4429
will henrique <willhc1984@gmail.com>
2022-08-22 14:42:47 -0300

    Inicio de projetos com IDE STS

---
+++ src/main/java/com/springbootmvc/ProjetoSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootmvc/ProjetoSpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
